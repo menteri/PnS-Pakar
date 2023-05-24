406812d11fe875f4f582364a8a8014c956e6bc7a
menteri <kokoantoko112@gmail.com>
2023-05-24 20:51:25 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sipaba</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="title">
+    <h2>Implementasi Sistem Pakar Dengan Metode Centairly</h2>
+    <h2>Faktor dan forward chaining </h2>
+  </div>
+
+  <div class="content">
+
+    <div class="desc">
+      <h2>UPT TIK UNSIKA</h2>
+      <p>
+      Pelayanan informasi serta pengelolaannya dengan terintegrasi satu sama lain
+menjadi sangat penting di setiap organisasi atau Lembaga
+      </p>
+
+
+      <a href="diagnosa.php"> TEST >> </a>
+    </div>
+  </div>
+</body>
+
+</html>
